956c74aafa489e7773844be68183fc5095c21233
ShahriyarHussain <shossainshihab36@gmail.com>
2021-11-16 01:47:03 +0600

    Add Spinner and 2nd Activity

--- app/src/main/java/com/example/broadcast/MainActivity.java
+++ app/src/main/java/com/example/broadcast/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.broadcast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected int SELECTED_OPTION = 0;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.broadcasts,
+                android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
+
 }
