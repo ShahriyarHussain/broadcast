39318e148cc0d5d4a33888a11d31294900efadda
ShahriyarHussain <shossainshihab36@gmail.com>
2021-11-18 01:48:40 +0600

    Add wifi and custom broadcaster

--- app/src/main/java/com/example/broadcast/FinalActivity.java
+++ app/src/main/java/com/example/broadcast/FinalActivity.java
@@ -1,23 +1,20 @@
 package com.example.broadcast;
 
-import android.app.Activity;
-import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.BatteryManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
-
 public class FinalActivity extends AppCompatActivity {
 
-    private BroadcastReceiver br;
     TextView showStatus;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -25,17 +22,29 @@ public class FinalActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstances) {
         super.onCreate(savedInstances);
         setContentView(R.layout.activity_final);
-
-        br = new MyBroadcastReceiver();
         showStatus = findViewById(R.id.statusTextView);
         String message = getIntent().getStringExtra(MainActivity.ACTIVITY_MESSAGE);
         if (message != null) {
-            Log.d("ss", "onCreate: part2");
+            ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+            if (activeNetwork != null && activeNetwork.isConnectedOrConnecting()) {
+                if (cm.isActiveNetworkMetered()) {
+                    showStatus.setText(R.string.connectionMsg1);
+                } else {
+                    showStatus.setText(R.string.connectionMsg2);
+                }
+            } else {
+                showStatus.setText(R.string.connectionMsg3);
+            }
+
         } else {
             String[] messages = getIntent().getStringArrayExtra(UserActivity.SECOND_ACTIVITY_MESSAGE);
             if (messages[1].startsWith("Sys")) {
                 showBatterNotification(messages[0]);
             } else {
+                MyBroadcastReceiver mbr = new MyBroadcastReceiver();
+                IntentFilter filter = new IntentFilter(messages[0]);
+                this.registerReceiver(mbr, filter);
                 showStatus.setText(new StringBuilder().append(messages[0]).append(" ").append(messages[1]));
             }
         }
@@ -43,16 +52,15 @@ public class FinalActivity extends AppCompatActivity {
 
     private void showBatterNotification(String userInput) {
         IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = this.registerReceiver(br, filter);
-        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging =
-                status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                        status == BatteryManager.BATTERY_STATUS_FULL;
+        Intent batteryStatus = this.registerReceiver(null, filter);
+
         int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
         int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
         float batteryPercent = level * 100 / (float)scale;
+
         showStatus.setText(new StringBuilder().append("Battery Status:").append(batteryPercent).
-                append("% and ").append(isCharging ? "Charging" : "Not Charging").
-                append(System.lineSeparator()).append("Your Entered Value: ").append(userInput));
+                append("% and ").append(batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS,-1)
+                == BatteryManager.BATTERY_STATUS_CHARGING ? "Charging" : "Not Charging").
+                append(System.lineSeparator()).append("Your Entered: ").append(userInput));
     }
 }
--- app/src/main/java/com/example/broadcast/UserActivity.java
+++ app/src/main/java/com/example/broadcast/UserActivity.java
@@ -23,10 +23,12 @@ public class UserActivity extends AppCompatActivity {
         EditText userInputField = findViewById(R.id.userInputField);
         TextView textView = findViewById(R.id.textView);
         Button nextButton = findViewById(R.id.nextBtn);
+        textView.setText(intentMessage);
+        if (intentMessage.startsWith("Sys")) userInputField.setHint("Enter Battery Percentage");
+        else userInputField.setHint("Enter Custom message");
 
         nextButton.setOnClickListener(v -> {
             String value = userInputField.getText().toString();
-            textView.setText(value);
             Intent intent = new Intent(this, FinalActivity.class).putExtra(SECOND_ACTIVITY_MESSAGE,new String[]{value, intentMessage});
             startActivity(intent);
         });
